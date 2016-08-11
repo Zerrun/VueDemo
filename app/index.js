2c48d01b29a8f13f17ef4fe07315d3ba7b045d20
errun <ZQCerrun@gmail.com>
2016-08-11 14:55:18 +0800

    先用官方给的生成器去做另一个结合 webpack 的

---
+++ app/index.js
@@ -0,0 +1,6 @@
+var app  = document.createElement('div');
+app.innerHTML = '<h1>Hello World 3</h1>';
+app.innerHTML = '<div id="app">{{ message }}</div>'
+app.innerHTML = '<script src="../vue.js"></script>'
+document.body.appendChild(app);
+
