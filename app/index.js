d623bff5b2091310cecbb7f1e8c96fe9d9f8c5f7
errun <ZQCerrun@gmail.com>
2016-08-15 14:36:30 +0800

    动态绑定

--- app/index.js
+++ app/index.js
@@ -1,26 +1,31 @@
 
 var app  = document.createElement('div');
-app.innerHTML = '<div id="example"><child my-message="hello!"></child></div>';
+app.innerHTML = '<div id="example"><parent-component :pmessage="message"></parent-component></div>';
 document.body.appendChild(app);
 
 var Vue = require("vue")
 
-var Child = Vue.extend({
-    template: '<div>A Child component !</div>'
+var Child = Vue.extend ({
+    // props: ['myMessage'],
+    template: '<div><input v-model="parentMsg"><br><child v-bind:my-message="parentMsg"></child></div>'
 })
 
-Vue.component('child', {
-    props: ['myMessage'],
-    template: '<h1>{{ myMessage }}</h1>'
-})
+// <template id="Child">
+//     <input v-model="msg">
+//     <button v-on:click="notify">Dispatch Event</button>
+// </template>
 
 Vue.component('parent-component', {
-    template: '<my-component></my-component>',
-    components: {'my-component': Child}
+    props: ['pmessage'],
+    template: '<div id="app"><h1>{{ pmessage }}</h1><input v-model="pmessage"></div>',
+    // components: {'Child': Child},
 })
 
 new Vue({
-  el: '#example'
+  el: '#example',
+  data: {
+      message: 'Hello Vue.js!'
+  }
 })
 
 
